6f60148413c99ba75b9d0b06e439d77dbcfe45e5
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-09-19 18:44:37 -0500

    User model created.

---
+++ src/main/java/com/ryancode/models/Game.java
@@ -0,0 +1,4 @@
+package com.ryancode.models;
+
+public class Game {
+}
---
+++ src/main/java/com/ryancode/models/User.java
@@ -0,0 +1,53 @@
+package com.ryancode.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Userid;
+
+    private String username;
+
+    private String pwHash;
+    @OneToMany
+    private List<Game> games;
+
+    public User() {}
+
+    //TODO replace pwHash with encoder
+    public User(Integer userid, String username, String password) {
+        Userid = userid;
+        this.username = username;
+        this.pwHash = pwHash;
+    }
+
+    public Integer getUserid() {
+        return Userid;
+    }
+
+    public void setUserid(Integer userid) {
+        Userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwHash() {
+        return pwHash;
+    }
+
+    public void setPwHash(String pwHash) {
+        this.pwHash = pwHash;
+    }
+
+}

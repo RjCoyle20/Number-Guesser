a3181d260c73412952685160726c2bc238d2742d
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-09-20 19:58:47 -0500

    Get user method is working. Tested with Postman.

--- src/main/java/com/devryan/controllers/UserController.java
+++ src/main/java/com/devryan/controllers/UserController.java
@@ -2,10 +2,13 @@ package com.devryan.controllers;
 
 import com.devryan.data.UserRepository;
 import com.devryan.models.User;
+import com.devryan.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,7 +21,17 @@ public class UserController {
 //    @Autowired
 //    private UserRepository userRepository;
 
-    @GetMapping("/display")
-    public List<User> getUser(){ return }
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/display/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable Integer id){
+        User user = userService.getUserById(id);
+        if (user == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(user);
+    }
 
 }

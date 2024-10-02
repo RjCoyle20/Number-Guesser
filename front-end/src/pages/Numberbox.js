68001d408f09d5922d77bb13d686c1db989a1954
RjCoyle20 <138418419+RjCoyle20@users.noreply.github.com>
2024-10-01 20:25:54 -0500

    Added submit button and set kGuesses.

--- front-end/src/pages/Numberbox.js
+++ front-end/src/pages/Numberbox.js
@@ -1,16 +1,45 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { useState } from 'react'
+import { Button, Form } from 'react-bootstrap';
 
 export default function Numberbox() {
+  
+  const [guesses, setGuesses] = useState ({guesses: 0})
+  const handleInputChange = (event) => {
+    const {name, value} = event.target
+    setGuesses({
+      ...guesses,
+      [name]: value
+    })
+    console.log(guesses)
+  };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+
+  //   console.log(guesses)
+
+  //   try {
+  //     const response = await fetch("http://localhost:8080/")
+  //   } catch (error) {
+      
+  //   }
+  // }
   return (
-    <Box
+    <Form onSubmit={handleInputChange}
       component="form"
       sx={{ '& > :not(style)': { m: 1, width: '25ch', } }}
       noValidate
       autoComplete="off"
     >
       <TextField id="filled-basic" label="Filled" variant="outlined" />
-    </Box>
+      <div>
+      <Button variant='primary' type='submit' className='button'>
+        Submit
+      </Button>
+      </div>
+    </Form>
   );
 }
